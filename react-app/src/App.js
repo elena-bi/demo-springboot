da4b84ce5851b39393b757f134c845d038becb03
elena-bi <bisericescuelena@gmail.com>
2021-06-14 12:04:28 +0300

    Add useEffect to update message only once

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,16 +2,39 @@ import './App.css';
 import React, {useState, useEffect } from 'react';
 
 const helloWorld = "Hello World";
-
+let shouldUpdate = true;
 
 function App() {
   const [message, setMessage] = useState(helloWorld);
 
+  useEffect(() => {
+    if (shouldUpdate) {
+      setMessage("New value");
+    }
+    return function () {
+      shouldUpdate = false;
+    }
+  })
+  class MessageContainer extends React.Component {
+    render() {
+      return (
+        <div>
+          {message}
+        </div>
+      )
+    }
+  }
+
   return (
     <div className="App">
-      {message}
+      <MessageContainer/>
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
