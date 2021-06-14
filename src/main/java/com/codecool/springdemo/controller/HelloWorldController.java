b2a9e860961495ca73c12511d1040d1a5c0b4cba
elena-bi <bisericescuelena@gmail.com>
2021-06-14 10:53:42 +0300

    Added controller to display Hello World

---
+++ src/main/java/com/codecool/springdemo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demospringboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    private String helloWorld = "Hello World!"; // could use a class for it
+
+    @GetMapping("/")
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+}
