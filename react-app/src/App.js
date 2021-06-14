b5c0de45fa40ad0e5d2e8b9d18fc8fcd91215c71
elena-bi <bisericescuelena@gmail.com>
2021-06-14 11:51:50 +0300

    Add message variable to use as a react hook

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import React, {useState, useEffect } from 'react';
 
 const helloWorld = "Hello World";
 
+
 function App() {
+  const [message, setMessage] = useState(helloWorld);
+
   return (
     <div className="App">
-      {helloWorld}
+      {message}
     </div>
   );
 }
