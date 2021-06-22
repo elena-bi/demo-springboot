a84f1c794bf584c8592bbc813b2be618b6a63eb8
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-22 14:58:11 +0300

    Add fetch to /api/hello to show string in browser

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,17 +3,18 @@ import React, {useState, useEffect } from 'react';
 
 const helloWorld = "Hello World from React initial state";
 let shouldUpdate = true;
+const helloApiEndpoint  = "http://localhost:8080/api/hello";
 
-async function doFetchWithGet(url, callback) {
-  const headersAndBody = {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-      accept: 'text/plain',
+async function doFetchWithGet(url) {
+    const headersAndBody = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+      Accept: 'text/plain',
     }
   };
   const response = await fetch(url, headersAndBody);
-
+  return response;
 }
 
 
@@ -22,12 +23,34 @@ function App() {
 
   useEffect(() => {
     if (shouldUpdate) {
-      // setMessage("New value");
+      // setMessage("Hello with new value from useEffect");
+
+      let responseStatus = 0;
+      doFetchWithGet(helloApiEndpoint)
+        .then(
+          (response) => {
+            responseStatus = response.status;     
+            // console.log(responseStatus+">> Got response from API: ");
+            return response.text();
+          }        
+        ).then(
+          (responseText) => {
+            setMessage(responseText);
+          }
+        ).catch(
+          (error) => {
+            if (responseStatus !== 0) { console.log(responseStatus); }
+            console.log(error);
+          }  
+        );
+      
     }
     return function () {
       shouldUpdate = false;
     }
   })
+
+   
   class MessageContainer extends React.Component {
     render() {
       return (
