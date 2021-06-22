34f6ae0f2473f31243fdc09099144fe9153c58d2
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-22 14:56:35 +0300

    Add CrossOrigin support server-side

--- src/main/java/com/codecool/springdemo/controller/HelloWorldController.java
+++ src/main/java/com/codecool/springdemo/controller/HelloWorldController.java
@@ -1,8 +1,10 @@
 package com.codecool.springdemo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("http://localhost:3000")
 @RestController
 public class HelloWorldController {
     private String helloWorld = "Hello World!"; // could use a class for it
