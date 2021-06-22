c847bfba53c57a1f6780ca394ccd78065b7d60a2
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-22 17:32:28 +0300

    Add Constructor for Controller which injects needed beans

--- src/main/java/com/codecool/springdemo/controller/HelloWorldController.java
+++ src/main/java/com/codecool/springdemo/controller/HelloWorldController.java
@@ -1,14 +1,30 @@
 package com.codecool.springdemo.controller;
 
+import com.codecool.springdemo.model.Message;
+import com.codecool.springdemo.repositories.MessageRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 public class HelloWorldController {
-    private String helloWorld = "Hello World!"; // could use a class for it
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
+    private String helloWorld;// could use a class for it
+    private Logger logger;
+
+    private MessageRepository messageRepository;
+
+    @Autowired
+    public HelloWorldController(@Value("${helloWorldMessage}")
+                                String helloWorld,
+
+                                MessageRepository messageRepository) {
+        this.helloWorld = helloWorld;
+        this.messageRepository = messageRepository;
+        this.logger = LoggerFactory.getLogger(this.getClass());
+    }
 
     @GetMapping("/")
     public String getHelloWorld() {
